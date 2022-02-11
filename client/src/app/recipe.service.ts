25845483a53f47a8850aa3f917f6b02dbe30f202
Mac <mac@Macs-MacBook-Pro.local>
2022-02-11 15:46:40 +0800

    add heroku

--- client/src/app/recipe.service.ts
+++ client/src/app/recipe.service.ts
@@ -11,10 +11,10 @@ export class RecipeService {
   constructor(private http: HttpClient) { }
 
   getAllRecipes(): Observable<any> {
-    return this.http.get<Recipe[]>("http://localhost:8080/api/recipes")
+    return this.http.get<Recipe[]>("/api/recipes")
   }
 
   getRecipe(recipeId: string): Observable<any>{
-    return this.http.get<Recipe>("http://localhost:8080/api/recipe/" + recipeId)
+    return this.http.get<Recipe>("/api/recipe/" + recipeId)
   }
 }
